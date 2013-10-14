d43c84841531410242263f74d9c714cfc93980c4
mglagola <mark.glagola@gmail.com>
2013-10-14 13:23:05 -0500

    some what working game

--- src/com/cssdpu/hangman/MainActivity.java
+++ src/com/cssdpu/hangman/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cssdpu.hangman;
 
+import java.util.Random;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,13 +14,20 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	private EditText mEditText;
-	private TextView mGuessedTextView;
+	private TextView mGuessedTextView, mCorrectlyGuessedTextView;
 	private String mGuessedString = "";
+	
+	private String mCurrentWord;
+
+	private static final String[] WORDS = new String[] {
+		"Belly", "Runner", "Chicago", "DePaul", "Fake"
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		resetGame();
 	}
 
 	@Override
@@ -25,6 +35,32 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    case R.id.action_restart:
+	    	resetGame();
+	        return true;
+	    default:
+	        return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private void resetGame() {
+		mCurrentWord = null;
+		mGuessedString = "";
+		getEditText().setText("");
+		getGuessedTextView().setText("");
+		getCorrectlyGuessedTextView().setText("");
+		//reset other stuff here
+		
+		//Generate new word
+		mCurrentWord = generateRandomWord();
+		
+		String underscoreWord = getCurrentWord().replaceAll("[A-Za-z]", "_");
+		getCorrectlyGuessedTextView().setText(underscoreWord);
 	}
 	
 	private void showToast(String message) {
@@ -39,6 +75,12 @@ public class MainActivity extends Activity {
 		return false;
 	}
 	
+	private boolean isCorrectGuess(String text) {
+		if (getCurrentWord().contains(text))
+			return true;
+		return false;
+	}
+	
 	public void onSubmitPress(View view) {
 		final String text = getEditText().getText().toString().toUpperCase();
 		getEditText().setText(""); //erase text
@@ -48,8 +90,21 @@ public class MainActivity extends Activity {
 			return; //don't penalize - just have them guess again
 		}
 		
-		//add to guess string
+		//add to guess string (doesn't matter if wrong or right guess)
 		addToGuessedString(text);
+		
+		if (isCorrectGuess(text)) {
+			showToast("Correct!");
+			
+			StringBuilder currentCorrectText = new StringBuilder(getCorrectlyGuessedTextView().getText().toString());
+			final String currWord = getCurrentWord();
+			int index = currWord.indexOf(text);
+			while (index != -1) {
+				currentCorrectText.setCharAt(index, text.charAt(0));
+				index = currWord.indexOf(text, index+1);
+			}
+			getCorrectlyGuessedTextView().setText(currentCorrectText);
+		}
 		
 		
 	}
@@ -62,10 +117,29 @@ public class MainActivity extends Activity {
 		getGuessedTextView().setText(mGuessedString);
 	}
 	
+	private String generateRandomWord() {
+		int rand = new Random().nextInt(WORDS.length);
+		return WORDS[rand].toUpperCase();
+	}
+	
+	
+	/*
+	 * Getter methods below
+	 */
+	public String getCurrentWord() {
+		return mCurrentWord;
+	}
+	
 	public TextView getGuessedTextView() {
 		if (mGuessedTextView == null)
 			mGuessedTextView = (TextView) findViewById(R.id.guessedTextView);
 		return mGuessedTextView;
+	}
+	
+	public TextView getCorrectlyGuessedTextView() {
+		if (mCorrectlyGuessedTextView == null)
+			mCorrectlyGuessedTextView = (TextView) findViewById(R.id.correctlyGuessedTextView);
+		return mCorrectlyGuessedTextView;
 	}
 	
 	public EditText getEditText() {
